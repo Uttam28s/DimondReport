f266df4b28e614f16f27be8c5784e2daf504497d
Uttam28s <umoradiya31@gmail.com>
2022-07-26 10:28:47 +0530

    change port number

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const env = require("dotenv").config().parsed;
 const passport = require("passport");
 const { mainRoutes } = require("./Routes/index");
-const port = 3003;
+const port = process.env.PORT || 3003;
 const cors = require("cors");
 
 const dConnection =
